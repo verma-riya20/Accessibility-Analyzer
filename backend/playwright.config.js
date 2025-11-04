2d8556d24646f62ebebf3bc9e5d87132fc003f24
Riya Verma <157815520+verma-riya20@users.noreply.github.com>
2025-11-05 01:04:33 +0530

    Update playwright.config.js

--- backend/playwright.config.js
+++ backend/playwright.config.js
@@ -1,14 +1,16 @@
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
   use: {
     headless: true,
-    chromiumSandbox: false,
-    args: [
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-gpu",
-      "--disable-dev-shm-usage",
-    ],
-  },
+    launchOptions: {
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-gpu',
+        '--disable-dev-shm-usage'
+      ]
+    }
+  }
 };
 
-export default config;
+module.exports = config;
